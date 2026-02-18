5cc1ee7fa5f642aee69d7e558649688b7f8b6d54
DionDi <diondiq@gmail.com>
2026-02-18 22:05:48 +0700

    fix: добавлен комментарий по замечанию ревьюера

--- src/ObjectOrientedPractics/Model/Item.cs
+++ src/ObjectOrientedPractics/Model/Item.cs
@@ -230,6 +230,9 @@ namespace ObjectOrientedPractics.Model
         /// <returns>Строку с информацией об предмете.</returns>
         public override string ToString() => $"{Name}";
 
+        /// <summary>
+        /// Тестовый метод для PR.
+        /// </summary>
         private static void TestMethodForPR()
         {
             MessageBox.Show("Этот метод добавлен для лабораторной работы по Pull Request");
