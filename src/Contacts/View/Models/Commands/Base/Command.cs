e590cd6bd998da61b2799483df676f822ae86d60
DionDi <diondiq@gmail.com>
2026-02-19 20:27:44 +0700

    2 feat(MVVM): добавлены базовые файлы MVVM

---
+++ src/Contacts/View/Models/Commands/Base/Command.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace View.Models.Commands.Base
+{
+    /// <summary>
+    /// Базовый класс для команд.
+    /// </summary>
+    public abstract class Command : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public abstract bool CanExecute(object parameter);
+        public abstract void Execute(object parameter);
+    }
+}
